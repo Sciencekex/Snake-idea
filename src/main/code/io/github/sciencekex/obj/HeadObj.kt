ab014eb0b734899c631c67e7a2338d30ec535b8c
椎名潤七 <newbingtochatgpt233@outlook.com>
2024-11-23 17:22:12 +0800

    Kt重构解决了部分困难!!!!!

--- src/main/code/io/github/sciencekex/obj/HeadObj.kt
+++ src/main/code/io/github/sciencekex/obj/HeadObj.kt
@@ -1,137 +1,112 @@
-package io.github.sciencekex.obj;
+package io.github.sciencekex.obj
 
-import io.github.sciencekex.GameWin;
-import io.github.sciencekex.utils.GameUtils;
+import io.github.sciencekex.GameWin
+import io.github.sciencekex.utils.GameUtils
+import io.github.sciencekex.utils.GameUtils.drawWord
+import java.awt.Color
+import java.awt.Graphics
+import java.awt.Image
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
 
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.util.List;
-
-public class HeadObj extends GameObj{
+class HeadObj(img: Image?, x: Int, y: Int, frame: GameWin?) : GameObj(img, x, y, frame) {
     //方向 up down left right
-    public String direction="right";
+    var direction: String = "right"
 
-    public String getDirection() {
-        return direction;
-    }
-
-    public void setDirection(String direction) {
-        this.direction = direction;
-    }
-
-    public HeadObj(Image img, int x, int y, GameWin frame) {
-        super(img, x, y, frame);
+    init {
         //键盘监听事件
-        this.frame.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                changeDirection(e);
+        this.frame.addKeyListener(object : KeyAdapter() {
+            override fun keyPressed(e: KeyEvent) {
+                changeDirection(e)
             }
-        });
+        })
     }
 
     //控制移动方向
-    public void changeDirection(KeyEvent e){
-        switch(e.getKeyCode()){
-            case KeyEvent.VK_UP:
-                if (!"down".equals(direction)){
-                    direction="up";
-                }
-                break;
-            case KeyEvent.VK_DOWN:
-                if (!"up".equals(direction)){
-                    direction="down";
-                }
-                break;
-            case KeyEvent.VK_LEFT:
-                if (!"right".equals(direction)){
-                    direction="left";
-                }
-                break;
-            case KeyEvent.VK_RIGHT:
-                if (!"left".equals(direction)){
-                    direction="right";
-                }
-                break;
-            default:
-                System.out.println("输入有误");
+    fun changeDirection(e: KeyEvent) {
+        when (e.getKeyCode()) {
+            KeyEvent.VK_UP -> if ("down" != direction) {
+                direction = "up"
+            }
+
+            KeyEvent.VK_DOWN -> if ("up" != direction) {
+                direction = "down"
+            }
+
+            KeyEvent.VK_LEFT -> if ("right" != direction) {
+                direction = "left"
+            }
+
+            KeyEvent.VK_RIGHT -> if ("left" != direction) {
+                direction = "right"
+            }
+
+            else -> println("输入有误")
         }
     }
-    //蛇的移动
-    public void move(){
-        //蛇身体的移动
-        List<BodyObj> bodyObjList=this.frame.bodyObjList;
-        for(int i=bodyObjList.size()-1;i>=1;i--){
-            bodyObjList.get(i).x=bodyObjList.get(i-1).x;
-            bodyObjList.get(i).y=bodyObjList.get(i-1).y;
-            //碰撞判断
-            if(this.x==bodyObjList.get(i).x&&this.y==bodyObjList.get(i).y){
-                Graphics g;
-                if(this.frame.g!=null){
-                    g=this.frame.g;
-                }else{
-                    g=this.frame.getGraphics();
-                }
-                GameUtils.drawWord(g, "请自行结束", Color.RED, 60, 100, 100);
-                //bug，Runtime.getRuntime().halt(0);
 
+    //蛇的移动
+    fun move() {
+        //蛇身体的移动
+        val bodyObjList = this.frame.bodyObjList
+        for (i in bodyObjList.size - 1 downTo 1) {
+            bodyObjList.get(i)!!.x = bodyObjList.get(i - 1)!!.x
+            bodyObjList.get(i)!!.y = bodyObjList.get(i - 1)!!.y
+            //碰撞判断
+            if (this.x == bodyObjList.get(i)!!.x && this.y == bodyObjList.get(i)!!.y) {
+                var g: Graphics
+                if (this.frame.g != null) {
+                    g = this.frame.g!!
+                } else {
+                    g = this.frame.getGraphics()
+                }
+                drawWord(g, "请自行结束", Color.RED, 60, 100, 100)
+
+                //bug，Runtime.getRuntime().halt(0);
             }
         }
-        bodyObjList.get(0).x=this.x;
-        bodyObjList.get(0).y=this.y;
+        bodyObjList.get(0)!!.x = this.x
+        bodyObjList.get(0)!!.y = this.y
 
         //蛇头的移动
-        switch (direction){
-            case "left":
-                x-=width;
-                break;
-            case "right":
-                x+=width;
-                break;
-            case "up":
-                y-=height;
-                break;
-            case "down":
-                y+=height;
-                break;
-                default:
-                    System.out.println("输入有误");
+        when (direction) {
+            "left" -> x -= width
+            "right" -> x += width
+            "up" -> y -= height
+            "down" -> y += height
+            else -> println("输入有误")
         }
     }
 
 
-    @Override
-    public void paintSelf(Graphics g) {
-        super.paintSelf(g);
+    override fun paintSelf(g: Graphics) {
+        super.paintSelf(g)
         //食物
-        FoodObj food=this.frame.foodObj;
+        val food = this.frame.foodObj
         //身体最后一节的坐标
-        Integer newX=null;
-        Integer newY=null;
+        var newX: Int? = null
+        var newY: Int? = null
 
-        if(this.x==food.x&&this.y==food.y){
-            this.frame.foodObj=food.getFood();
+        if (this.x == food.x && this.y == food.y) {
+            this.frame.foodObj = food.getFood()
             //获取蛇身的最后一个元素
-            BodyObj lastBody=this.frame.bodyObjList.get(this.frame.bodyObjList.size()-1);
-            newX= lastBody.x;
-            newY= lastBody.y;
-
+            val lastBody = this.frame.bodyObjList.get(this.frame.bodyObjList.size - 1)
+            newX = lastBody!!.x
+            newY = lastBody.y
         }
-        move();
-        if(newX!=null&&newY!=null){
-            this.frame.bodyObjList.add(new BodyObj(GameUtils.bodyImg,newX,newY,this.frame));
+        move()
+        if (newX != null && newY != null) {
+            this.frame.bodyObjList.add(BodyObj(GameUtils.bodyImg, newX, newY, this.frame))
         }
         //越界处理
-        if (x<=0) {
-            x = 600;
-        }else if(x>600){
-            x=0;
-        }else if(y<0){
-            y=600;
-        }else if(y>600){
-            y=0;
+        if (x <= 0) {
+            x = 600
+        } else if (x > 600) {
+            x = 0
+        } else if (y < 0) {
+            y = 600
+        } else if (y > 600) {
+            y = 0
         }
-
     }
 }
