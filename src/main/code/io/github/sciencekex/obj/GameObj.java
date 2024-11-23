785fd3041b88258b79b54f507579f02d790c24a3
椎名潤七 <newbingtochatgpt233@outlook.com>
2024-11-23 18:22:08 +0800

    Kt重构,我终于解决了大部分困难!!!!!

--- src/main/code/io/github/sciencekex/obj/GameObj.java
+++ src/main/code/io/github/sciencekex/obj/GameObj.java
@@ -5,85 +5,31 @@ import io.github.sciencekex.GameWin;
 import java.awt.*;
 
 public class GameObj {
-    //定义图片
-    Image img;
-    //坐标
-    int x,y;
-    int width=30,height=30;
-    //窗口类的引用
-    GameWin frame;
+    // 定义图片及其属性
+    private Image img;
+    private int x, y, width = 30, height = 30;
+    private GameWin frame;
 
-    //一键生成get和set方法
-    public Image getImg() {
-        return img;
-    }
-
-    public void setImg(Image img) {
-        this.img = img;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public GameWin getFrame() {
-        return frame;
-    }
-
-    public void setFrame(GameWin frame) {
-        this.frame = frame;
-    }
-
-    public GameObj() {
-    }
-
+    // 构造方法
+    public GameObj() {}
     public GameObj(Image img, int x, int y, GameWin frame) {
-        this.img = img;
-        this.x = x;
-        this.y = y;
-        this.frame = frame;
+        this(img, x, y, 30, 30, frame);
     }
-
     public GameObj(Image img, int x, int y, int width, int height, GameWin frame) {
-        this.img = img;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.frame = frame;
+        this.img = img; this.x = x; this.y = y;
+        this.width = width; this.height = height; this.frame = frame;
     }
 
-    //绘制自身
-    public void paintSelf(Graphics g)
-    {
-        g.drawImage(img,x,y,null);
+    // Getter和Setter方法（使用一行定义）
+    public Image getImg() { return img; } public void setImg(Image img) { this.img = img; }
+    public int getX() { return x; } public void setX(int x) { this.x = x; }
+    public int getY() { return y; } public void setY(int y) { this.y = y; }
+    public int getWidth() { return width; } public void setWidth(int width) { this.width = width; }
+    public int getHeight() { return height; } public void setHeight(int height) { this.height = height; }
+    public GameWin getFrame() { return frame; } public void setFrame(GameWin frame) { this.frame = frame; }
+
+    // 绘制自身
+    public void paintSelf(Graphics g) {
+        if (img != null) g.drawImage(img, x, y, null);
     }
 }
